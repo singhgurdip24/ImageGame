02622e9c11f2aee398a37f935666f15ec75235d9
Gurdip Singh <singh.gurdip24@gmail.com>
2018-10-31 23:23:14 -0400

    Implemented choose file option.

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ function userClicked() {
 //displayTime(10);
 
 function displayTime(timeleft){
+  showImage();
   $('#scenicBeauty').on("click",function(){
     console.log("image Clicked");
     timeleft=10;
+
   });
     var timeOutValue = setInterval(function(){
     timeleft--;
@@ -31,10 +33,31 @@ function displayTime(timeleft){
 
 }
 
+function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+  previewFile();
+
 function hideImage(){
       document.getElementById("scenicBeauty").style.display ='none';
 }
-
+function showImage(){
+  if(document.getElementById('scenicBeauty').style.display === "none")
+    document.getElementById('scenicBeauty').style.display='block';
+}
 function showButton(){
   if(document.getElementById('startClock').style.display === "none")
     document.getElementById('startClock').style.display='block';
